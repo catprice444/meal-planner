216b31808cc34fbce9613bf2b0f536e558f6599b
catprice444 <catprice444@gmail.com>
2021-04-06 10:27:00 -0400

    updated controllers

--- meal-planner-backend/app/controllers/categories_controller.rb
+++ meal-planner-backend/app/controllers/categories_controller.rb
@@ -1,2 +1,8 @@
 class CategoriesController < ApplicationController
+    def index 
+        categories = Category.all
+        render json: categories.to_json(:include => {
+            :meals => {:only => [:name, :ingredients]},
+          }, :except => [:updated_at, :created_at])
+    end 
 end
