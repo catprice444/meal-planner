1828a4efabdeaef1a3e2e48bda4fde25f5edd761
catprice444 <catprice444@gmail.com>
2021-04-07 11:02:10 -0400

    added initial fetch and fixed cors issue

--- meal-planner-backend/app/controllers/categories_controller.rb
+++ meal-planner-backend/app/controllers/categories_controller.rb
@@ -1,9 +1,7 @@
 class CategoriesController < ApplicationController
     def index 
         categories = Category.all
-        # render json: categories.to_json(:include => {
-        #     :meals => {:only => [:name, :ingredients]},
-        #   }, :except => [:updated_at, :created_at])
         render json: CategorySerializer.new(categories)
     end 
 end
+ 
--- meal-planner-frontend/index.js
+++ meal-planner-frontend/index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const mealsIndex = "http://localhost:3000/meals"
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetch(mealsIndex)
+    .then(result => result.json())
+    .then(meals => {
+        console.log(meals);
+    })
+})
