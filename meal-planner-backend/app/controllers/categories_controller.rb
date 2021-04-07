c66cc7340d10dbe4f0f6cd9c70850137ee154cb7
catprice444 <catprice444@gmail.com>
2021-04-07 09:44:23 -0400

    added serializer into controllers

--- meal-planner-backend/app/controllers/categories_controller.rb
+++ meal-planner-backend/app/controllers/categories_controller.rb
@@ -1,8 +1,9 @@
 class CategoriesController < ApplicationController
     def index 
         categories = Category.all
-        render json: categories.to_json(:include => {
-            :meals => {:only => [:name, :ingredients]},
-          }, :except => [:updated_at, :created_at])
+        # render json: categories.to_json(:include => {
+        #     :meals => {:only => [:name, :ingredients]},
+        #   }, :except => [:updated_at, :created_at])
+        render json: CategorySerializer.new(categories)
     end 
 end
