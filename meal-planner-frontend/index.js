81d2641774d29e53bacf912ef69684a3a8390954
catprice444 <catprice444@gmail.com>
2021-04-07 12:46:34 -0400

    added a create meal button to header

--- meal-planner-frontend/index.js
+++ meal-planner-frontend/index.js
@@ -1,10 +1,11 @@
 const mealsIndex = "http://localhost:3000/meals"
 const categoriesIndex = "http://localhost:3000/categories"
 const main = document.querySelector("main")
-const createMeal = document.getElementById("create-meals-form")
+const header = document.querySelector("header")
 
 document.addEventListener('DOMContentLoaded', ()=>{
     getMeals()
+    createMealbutton()
 })
 
 function getMeals(){
@@ -31,7 +32,32 @@ function getMeals(){
             main.appendChild(square);
 
         })
-        
-
     })
 }
+
+function createMealbutton(){
+    let button = document.createElement("button");
+    button.innerText = "Add a New Meal!"
+    button.addEventListener = ("click", event => addMeal(event));
+    header.append(button)
+}
+
+function addMeal(event){
+
+}
+
+{/* <form id="create-meals-form">
+            <h3>Create a New Meal!</h3>
+
+            <input id="input-name" type="text" name="name" value="" placeholder="Enter your meal name" class="input-name">
+            <textarea id="input-ingredients" name="ingredients" value="" placeholder="Enter the ingredients needed to make the meal"></textarea>
+            <select id="input-category" name="categories">
+                <option value="category1">Breakfast</option>
+                <option value="category2">Lunch</option>
+                <option value="category3">Dinner</option>
+                <option value="category4">Snacks</option>
+                <option value="category5">Drinks</option>
+            </select>
+            <input id= "create-button" type="submit" name="submit" value="Create New Meal" class="submit">
+
+        </form> */}
