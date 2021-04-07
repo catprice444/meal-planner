552053cdd0796a0250751cc6b60364def2ac74fd
catprice444 <catprice444@gmail.com>
2021-04-07 09:01:31 -0400

    removed instance variables

--- meal-planner-backend/app/controllers/meals_controller.rb
+++ meal-planner-backend/app/controllers/meals_controller.rb
@@ -1,12 +1,12 @@
 class MealsController < ApplicationController
     def index 
-        @meals = Meal.all
-        render json: @meals
+        meals = Meal.all
+        render json: meals
     end 
 
     def create
-        @meal.new(meal_params)
-        if @meal.save
+        meal = Meal.new(meal_params)
+        if meal.save
             render json: meal
         else 
             render json: {errors: meal.error.full_messages}
