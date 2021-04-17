bc31eafc5798f668d9f21f6ae8e85e158bd52c40
catprice444 <catprice444@gmail.com>
2021-04-17 08:17:46 -0400

    added meal and category class

--- meal-planner-frontend/src/meal.js
+++ meal-planner-frontend/src/meal.js
@@ -1,6 +1,8 @@
 class Meal {
-
-
-
-    
+    constructor({id, name, ingredients, category_id}){
+        this.id = id
+        this.name = name 
+        this.ingredients = ingredients
+        this.category_id = category_id
+    }  
 }
